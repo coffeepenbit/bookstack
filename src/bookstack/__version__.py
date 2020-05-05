cb24d6de2c641daa298abbacdbc37b7546634061
coffeepenbit <coffeepenbit@gmail.com>
2020-05-05 08:13:16 -0400

    Removed cruft

---
+++ src/bookstack/__version__.py
@@ -0,0 +1,7 @@
+__author__ = 'coffeepenbit'
+__description__ = "A Python wrapper for BookStack's API"
+__author_email__ = 'coffeepenbit@gmail.com'
+__maintainer__ = 'coffeepenbit'
+__title__ = 'bookstack'
+__url__ = "https://github.com/coffeepenbit/bookstack"
+__version__ = '0.1.0-alpha.2'
