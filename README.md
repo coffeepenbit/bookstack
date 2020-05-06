a1769f35dec74f2ee013f69b11d93f55e9da4466
coffeepenbit <coffeepenbit@gmail.com>
2020-05-06 06:22:19 -0400

    Added dynamic method generation

--- README.md
+++ README.md
@@ -15,16 +15,35 @@ To use BookStack's API, you'll need to get a token ID and secret.
 
 You can find how to get these values from your BookStack instance's doc page at `http[s]://<example.com>/api/docs`
 
+**Note**: Your account's user group must have API usage priveleges enabled.
+
 # Usage
 Once you've acquired your token ID and secret, you're ready to rock.
 
 ```python
-import bookstack
+>>> import bookstack
 
 # Input the appropriate values for these three variables
-base_url = 'http[s]://<example.com>'
-token_id = '<token_id>'
-token_secret = '<token_secret>'
+>>> base_url = 'http[s]://<example.com>'
+>>> token_id = '<token_id>'
+>>> token_secret = '<token_secret>'
 
-api = bookstack.BookStack(base_url, token_id=token_id, token_secret=token_secret)
+>>> api = bookstack.BookStack(base_url, token_id=token_id, token_secret=token_secret)
+```
+
+This wrapper *dynamically* generates its API calls at runtime. To have the wrapper generate the methods, use:
+
+```python
+>>> api.generate_api_methods()
+
+>>> api.available_api_methods
+{'get_books_export_pdf', 'get_shelves_list', 'post_books_create', 'get_docs_display', 'delete_shelves_delete', 'get_books_list', 'get_docs_json', 'delete_books_delete', 'get_books_read', 'get_shelves_read', 'put_books_update', 'get_books_export_plain_text', 'get_books_export_html', 'post_shelves_create', ...}
+```
+
+The above are then the methods available to you, for example:
+
+```python
+>>> books_list = api.get_books_list()
+>>> books_list['data'][0]['name']
+'Mathematics'
 ```
--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-about = {}
-with open(os.path.join(here, 'src', 'bookstack', '__init__.py'), 'r', encoding='utf-8') as f:
-    exec(f.read(), about)
-
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-install_requires = [
-    'requests'
+CLASSIFIERS = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
 ]
-
-extras_require = {
+EXTRAS_REQUIRE = {
     'dev': [
         'bump2version',
         'pylint',
@@ -25,24 +16,43 @@ extras_require = {
         'tox'
     ]
 }
+INSTALL_REQUIRES = [
+    'requests>= 2.18, < 3',
+    'cached_property >= 1.5, < 2',
+    'inflection == 0.4'
+]
 
+
+def get_about(here):
+    about = {}
+    about_info_path = os.path.join(here, 'src', 'bookstack', '__version__.py')
+    with open(about_info_path,'r', encoding='utf-8' ) as f:
+        exec(f.read(), about)
+
+    return about
+
+def get_long_description(here):
+    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    return long_description
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = get_about(here)
 
 setup(
     name=about['__title__'],
     version=about['__version__'],
     url=about['__url__'],
     description=about['__description__'],
-    long_description=long_description,
+    long_description=get_long_description(here),
     author=about['__author__'],
     author_email=about['__author_email__'],
     long_description_content_type='text/markdown',
     packages=find_packages('src'),    
     package_dir={'': 'src'},
-    install_requires=install_requires,
-    extras_require=extras_require,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    classifiers=CLASSIFIERS
 )
